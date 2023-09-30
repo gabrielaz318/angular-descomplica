2cf75fcf705cd9cac92a8577f1893c6a20a7a16c
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2023-09-30 12:33:58 -0300

    Mais arquivos de páginas criados para ter subrotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { ListarComponent } from './pages/listar/listar.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ManipulandoJsonComponent } from './pages/manipulando-json/manipulando-json.component';
+import { SubRouteComponent } from './pages/sub-route/sub-route.component';
+import { Page1Component } from './pages/sub-route/page1/page1.component';
+import { Page2Component } from './pages/sub-route/page2/page2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,7 +15,13 @@ const routes: Routes = [
   { path: 'listar', component: ListarComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'json', component: ManipulandoJsonComponent }
+  { path: 'json', component: ManipulandoJsonComponent },
+  { path: 'subroute', component: SubRouteComponent,
+    children: [
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component }
+    ]
+  }
 ];
 
 @NgModule({
