bf42f04bb3e9d7367d3b9db765935a00a53b6427
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2023-09-30 14:13:51 -0300

    Feita validação de email na página de login e cadastro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -12,10 +12,27 @@ export class CadastroComponent {
   private fb = inject(FormBuilder);
   addressForm = this.fb.group({
     name: [null, Validators.required],
-    email: [null, Validators.required],
+    email: [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(64),
+        Validators.email
+      ])
+    ],
     phone: [null, Validators.required],
     password: [null, Validators.required]
   });
+
+  email = this.addressForm.controls['email'];
+
+  getErrorMessage() {
+    if(this.email.hasError('required')) {
+      return 'O e-mail é obrigatório!';
+    }
+
+    return this.email.hasError('email') ? 'Você deve preecher o e-mail com um valor válido' : '';
+  }
+
 
   onSubmit(): void {
     alert('Entrou no onSubmit');
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -13,10 +13,24 @@ export class LoginComponent {
   private fb = inject(FormBuilder);
   addressForm = this.fb.group({
     email: [null, Validators.compose([
-      Validators.required, Validators.minLength(10), Validators.maxLength(64)])
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(64),
+        Validators.email
+      ])
     ],
     password: [null, Validators.required]
   });
+
+  email = this.addressForm.controls['email'];
+
+  getErrorMessage() {
+    if(this.email.hasError('required')) {
+      return 'O e-mail é obrigatório!';
+    }
+
+    return this.email.hasError('email') ? 'Você deve preecher o e-mail com um valor válido' : '';
+  }
 
   constructor(
     private AutorizacaoService: AutorizacaoService
